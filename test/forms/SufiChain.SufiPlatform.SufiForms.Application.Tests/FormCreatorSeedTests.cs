fcae8b621e79691339689f4cd4d6b415d0051d58
Pooria Shariatzadeh <pooria.shariatzadeh@gmail.com>
2026-09-15 21:27:37 +0330

    test(forms): cover private copilot seeding and renderer limitation guidance

---
+++ test/forms/SufiChain.SufiPlatform.SufiForms.Application.Tests/FormCreatorSeedTests.cs
@@ -0,0 +1,48 @@
+using NSubstitute;
+using Shouldly;
+using SufiChain.SufiPlatform.Localization;
+using SufiChain.SufiPlatform.SufiAI.Copilots.Copilots;
+using SufiChain.SufiPlatform.SufiForms.Data;
+using Volo.Abp.Data;
+using Xunit;
+
+namespace SufiChain.SufiPlatform.SufiForms;
+
+public class FormCreatorSeedTests
+{
+    [Fact]
+    public async Task Seed_is_private_tenant_aware_and_allows_only_definition_tools()
+    {
+        var definitions = new List<PlatformCopilotSeedDefinition>();
+        var seeder = Substitute.For<IPlatformCopilotDefinitionSeeder>();
+        var tenantId = Guid.NewGuid();
+        seeder.SeedAsync(Arg.Any<PlatformCopilotSeedDefinition>(), Arg.Any<DataSeedContext>(), Arg.Any<CancellationToken>())
+            .Returns(call =>
+            {
+                call.Arg<DataSeedContext>().TenantId.ShouldBe(tenantId);
+                definitions.Add(call.Arg<PlatformCopilotSeedDefinition>());
+                return Task.CompletedTask;
+            });
+        var contributor = new FormCreatorCopilotDataSeedContributor(seeder, Substitute.For<ILocalizationTextSeeder>());
+        await contributor.SeedAsync(new DataSeedContext(tenantId));
+        var definition = definitions.ShouldHaveSingleItem();
+        definition.Key.ShouldBe(PlatformCopilotKeys.SufiFormsFormCreator);
+        definition.IsPublic.ShouldBeFalse();
+        definition.PersistChatSession.ShouldBeFalse();
+        definition.RuntimeOptions.AllowedMcpToolNames.ShouldBe(new List<string>
+        {
+            "forms.get_designer_catalog", "forms.get_definition", "forms.create_definition", "forms.update_definition"
+        });
+        definition.RequiredContextKeys.ShouldBe(new List<string> { "operation", "definitionId", "phase", "proposal" });
+        foreach (var culture in new[] { "en", "fa", "ar", "es" })
+        {
+            FormCreatorCopilotSeedTexts.Texts.DisplayName[culture].ShouldNotBeNullOrWhiteSpace();
+            FormCreatorCopilotSeedTexts.Texts.SystemPrompt[culture].ShouldContain("RendererLimitations");
+            foreach (var name in definition.RuntimeOptions.AllowedMcpToolNames)
+            {
+                FormsMcpToolSeedTexts.Get(name).DisplayNames[culture].ShouldNotBeNullOrWhiteSpace();
+                FormsMcpToolSeedTexts.Get(name).Descriptions[culture].ShouldNotBeNullOrWhiteSpace();
+            }
+        }
+    }
+}
